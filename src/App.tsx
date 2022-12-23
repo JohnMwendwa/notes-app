18f33e6296fd6bf2a893d1e145ef16e55c76c90a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-23 11:37:16 +0300

    nest the editing page route inside details page

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,13 @@ function App() {
       {/* Home Route */}
       <Route path="/" element={<h1>Home</h1>} />
 
-      {/* Details page */}
-      <Route path="/:id" element={<h1>Details</h1>} />
+      {/* Details page Route*/}
+      <Route path="/:id">
+        <Route index element={<h1>Details</h1>} />
+
+        {/* Edit Page Route */}
+        <Route path="edit" element={<h1>Editing</h1>} />
+      </Route>
 
       {/* Return to homepage if route doesn't match specified routes */}
       <Route path="*" element={<Navigate to="/" />} />
