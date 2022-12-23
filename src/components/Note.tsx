bd2208f1c3c349de1484cab04ecbe88c8e4f0a69
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-23 17:55:23 +0300

    remove the notes markdown from home page

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,17 +1,12 @@
 import NoteProps from "../models/note";
 
-export default function Note({
-  title,
-  markdown,
-  tags,
-}: NoteProps): JSX.Element {
+export default function Note({ title, tags }: NoteProps): JSX.Element {
   return (
     <>
       <h2>{title}</h2>
       {tags.map((tag) => {
         return <button key={tag.id}>{tag.label}</button>;
       })}
-      <p>{markdown}</p>
     </>
   );
 }
