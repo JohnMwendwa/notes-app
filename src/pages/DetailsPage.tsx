d8b4bd7b92e0b3c03e54f10675e4ff5e02f823f2
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-23 20:51:45 +0300

    render the details page

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
-export default function DetailsPage() {
-  return <div>DetailsPage</div>;
+import NoteProps from "../models/note";
+
+const note: NoteProps = {
+  id: crypto.randomUUID(),
+  title: "Note 1",
+  markdown: "# Note 1",
+  tags: [
+    { id: crypto.randomUUID(), label: "CSS" },
+    { id: crypto.randomUUID(), label: "JS" },
+  ],
+};
+
+export default function DetailsPage(): JSX.Element {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  return (
+    <>
+      <h1>{note.title}</h1>
+      {note.tags.map((tag) => {
+        return <button key={tag.id}>{tag.label}</button>;
+      })}
+      <div>{note.markdown}</div>
+      <button onClick={() => navigate(-1)}>Go Back</button>
+    </>
+  );
 }
