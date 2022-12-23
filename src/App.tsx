f16f0d31900187aa49215b7eb2e299fae60c6c6c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-23 11:05:20 +0300

    create the details page route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { Route, Routes, Navigate } from "react-router-dom";
 function App() {
   return (
     <Routes>
+      {/* Home Route */}
       <Route path="/" element={<h1>Home</h1>} />
+
+      {/* Details page */}
+      <Route path="/:id" element={<h1>Details</h1>} />
 
       {/* Return to homepage if route doesn't match specified routes */}
       <Route path="*" element={<Navigate to="/" />} />
