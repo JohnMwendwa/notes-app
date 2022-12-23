f95256524fbb895594fa59d237d86924e7016ada
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-24 00:27:43 +0300

    create the note provider context

---
+++ src/context/NotesContext.tsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+import NoteProps from "../models/note";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const NotesContext = createContext([]);
+
+export function NotesContextProvider({ children }: Props) {
+  const [notes, setNotes] = useState<NoteProps[]>([]);
+
+  return (
+    <NotesContext.Provider value={{ notes, setNotes }}>
+      {children}
+    </NotesContext.Provider>
+  );
+}
