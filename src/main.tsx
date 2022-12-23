f7cc6467bd456b291aa25e93dd51392fd85b5266
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-24 00:31:22 +0300

    wrap the app with the Notes context provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+import { NotesContextProvider } from "./context/Notescontext";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <NotesContextProvider>
+        <App />
+      </NotesContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
