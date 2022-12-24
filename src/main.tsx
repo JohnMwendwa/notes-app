937155d3e21940a305c01fc6b45cf9af3433a910
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-24 19:43:48 +0300

    change file path

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-import { NotesContextProvider } from "./context/Notescontext";
+import { NotesContextProvider } from "./context/NotesContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
