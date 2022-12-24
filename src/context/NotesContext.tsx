9aa6aeb680976cfd72a38621e91d10160438e0dd
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-24 19:45:34 +0300

    create the contex file

--- src/context/NotesContext.tsx
+++ src/context/NotesContext.tsx
@@ -2,14 +2,37 @@ import { createContext, useContext, useState } from "react";
 
 import NoteProps from "../models/note";
 
+const dummy_notes: NoteProps[] = [
+  {
+    id: crypto.randomUUID(),
+    title: "Note 1",
+    markdown: "# Note 1",
+    tags: [
+      { id: crypto.randomUUID(), label: "CSS" },
+      { id: crypto.randomUUID(), label: "JS" },
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Note 2",
+    markdown: "# Note 2",
+    tags: [],
+  },
+];
+
 interface Props {
   children: React.ReactNode;
 }
 
-const NotesContext = createContext([]);
+interface Value {
+  notes: NoteProps[];
+  setNotes: React.Dispatch<React.SetStateAction<NoteProps[]>>;
+}
+
+const NotesContext = createContext({} as Value);
 
 export function NotesContextProvider({ children }: Props) {
-  const [notes, setNotes] = useState<NoteProps[]>([]);
+  const [notes, setNotes] = useState<NoteProps[]>(dummy_notes);
 
   return (
     <NotesContext.Provider value={{ notes, setNotes }}>
