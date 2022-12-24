79ce8ab7f81a57f549c34187aad9e056c2893290
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-24 23:59:03 +0300

    use refs to track changes

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+
 import NoteProps from "../models/note";
 
 interface NoteFormProps extends Partial<NoteProps> {
@@ -10,18 +12,23 @@ export default function NoteForm({
   markdown = "",
   tags = [],
 }: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const tagsRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+
   return (
     <form>
       <label htmlFor="title">
         Title :
-        <input type="text" id="title" />
+        <input type="text" id="title" required ref={titleRef} />
       </label>
       <label htmlFor="tags">
         Tags :
-        <input type="text" id="tags" />
+        <input type="text" id="tags" required ref={tagsRef} />
       </label>
       <label htmlFor="markdown">
-        Markdown :<textarea id="" rows={15}></textarea>
+        Markdown :
+        <textarea id="" rows={15} required ref={markdownRef}></textarea>
       </label>
       <button type="button">Cancel</button>
       <button type="submit">Save</button>
