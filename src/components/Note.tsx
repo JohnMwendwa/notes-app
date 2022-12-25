a4048cc63c4fb34f4a27482adab3375ccea8e138
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-25 12:02:40 +0300

    create the edit,delete and back button functionality

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,12 +1,29 @@
+import { useNavigate, Link } from "react-router-dom";
+
+import { useNotesContext } from "../context/NotesContext";
+
 import NoteProps from "../models/note";
 
-export default function Note({ title, tags }: NoteProps): JSX.Element {
+export default function Note({ id, title, tags }: NoteProps): JSX.Element {
+  const { onDeleteNote } = useNotesContext();
+  const navigate = useNavigate();
+
   return (
     <>
       <h2>{title}</h2>
       {tags.map((tag) => {
         return <button key={tag.id}>{tag.label}</button>;
       })}
+      <button onClick={() => navigate(`/${id}/edit`)}>Edit</button>
+      <button
+        onClick={() => {
+          onDeleteNote(id);
+          navigate("/");
+        }}
+      >
+        Delete
+      </button>
+      <button onClick={() => navigate(-1)}>Back</button>
     </>
   );
 }
