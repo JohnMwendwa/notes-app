6205da2d59aa50ed2c8d02d9de5469d1be07646d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-25 10:40:07 +0300

    find pass down data foor the note that is being edited

--- src/pages/EditNotePage.tsx
+++ src/pages/EditNotePage.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+
+import NoteForm from "../components/NoteForm";
+import { useNotesContext } from "../context/NotesContext";
 
 export default function EditNotePage() {
-  return <div>EditNotePage</div>;
+  const { id } = useParams();
+  const { notes, onUpdateNote } = useNotesContext();
+  const noteIdx = notes.findIndex((n) => n.id === id);
+  const note = notes[noteIdx];
+
+  return (
+    <div>
+      EditNotePage
+      <NoteForm {...note} onSubmit={onUpdateNote} />
+    </div>
+  );
 }
