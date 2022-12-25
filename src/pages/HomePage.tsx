b4024d06c911641669e7f297174ac8dc0d400765
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-25 12:10:27 +0300

    create a button to navigate to new note form

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,15 @@
-import NotesList from "../components/NotesList";
+import { useNavigate } from "react-router-dom";
 
+import NotesList from "../components/NotesList";
 import { useNotesContext } from "../context/NotesContext";
 
 export default function HomePage() {
   const { notes } = useNotesContext();
+  const navigate = useNavigate();
   return (
     <div>
       <h1>All Notes</h1>
+      <button onClick={() => navigate("/new")}>New</button>
 
       <NotesList notes={notes} />
     </div>
