d595799d0d445b265bf7b2ac31a6676d7b50c2fb
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-25 21:42:51 +0300

    style the NoteCard

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,12 +1,48 @@
+import styled from "styled-components";
+
 import NoteProps from "../models/note";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid black;
+  max-width: 300px;
+  margin: 20px;
+  padding: 20px 30px;
+  border-radius: 5px;
+  transition: 0.3s ease-in-out;
+
+  :hover {
+    background-color: #ddd;
+  }
+
+  h2 {
+    margin: 0;
+    margin-bottom: 10px;
+    color: blue;
+  }
+`;
+
+const Tags = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+
+  button {
+    margin-right: 3px;
+    margin-bottom: 5px;
+  }
+`;
 
 export default function NoteCard({ title, tags }: NoteProps) {
   return (
-    <>
+    <Wrapper>
       <h2>{title}</h2>
-      {tags.map((tag) => {
-        return <button key={tag.id}>{tag.label}</button>;
-      })}
-    </>
+      <Tags>
+        {tags.map((tag) => {
+          return <button key={tag.id}>{tag.label}</button>;
+        })}
+      </Tags>
+    </Wrapper>
   );
 }
