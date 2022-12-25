e1b6a1e56f8929849e2c634e717f943e4389940c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-25 14:26:01 +0300

    set global styles

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom";
+import styled, { createGlobalStyle } from "styled-components";
 
 import NoteProps from "../models/note";
 import NoteCard from "./NoteCard";
+
+const GlobalStyles = createGlobalStyle`
+body{
+  display:flex;
+  flex-direction:column;
+  min-height:100vh;
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+
+}
+`;
 
 export default function NotesList({
   notes,
@@ -10,6 +23,7 @@ export default function NotesList({
 }): JSX.Element {
   return (
     <>
+      <GlobalStyles />
       {notes.map((note) => {
         return (
           <Link to={`/${note.id}`} key={note.id}>
