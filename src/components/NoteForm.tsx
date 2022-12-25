86d5fd2b8b6b4a48c4b38e6e1249ac71d3ab4e5b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-25 09:45:05 +0300

    create a function to handle submitted data

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,9 +1,10 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import NoteProps from "../models/note";
 
 interface NoteFormProps extends Partial<NoteProps> {
-  onSubmit: (data: NoteFormProps) => void;
+  onSubmit: (data: Partial<NoteFormProps>) => void;
 }
 
 export default function NoteForm({
@@ -12,23 +13,57 @@ export default function NoteForm({
   markdown = "",
   tags = [],
 }: NoteFormProps) {
+  const value = tags.map((t) => t.label).join(" ");
+  const [newTags, setNewTags] = useState(value);
   const titleRef = useRef<HTMLInputElement>(null);
-  const tagsRef = useRef<HTMLInputElement>(null);
+  // const tagsRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
 
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    onSubmit({
+      title: titleRef.current?.value,
+      markdown: markdownRef.current?.value,
+      tags: newTags
+        .split(" ")
+        .map((t) => ({ label: t, id: crypto.randomUUID() })),
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="title">
         Title :
-        <input type="text" id="title" required ref={titleRef} />
+        <input
+          type="text"
+          id="title"
+          required
+          ref={titleRef}
+          defaultValue={title}
+        />
       </label>
       <label htmlFor="tags">
         Tags :
-        <input type="text" id="tags" required ref={tagsRef} />
+        <input
+          type="text"
+          id="tags"
+          required
+          value={newTags}
+          onChange={(e) => setNewTags(e.target.value)}
+        />
       </label>
       <label htmlFor="markdown">
         Markdown :
-        <textarea id="" rows={15} required ref={markdownRef}></textarea>
+        <textarea
+          id=""
+          rows={15}
+          required
+          ref={markdownRef}
+          defaultValue={markdown}
+        ></textarea>
       </label>
       <button type="button">Cancel</button>
       <button type="submit">Save</button>
