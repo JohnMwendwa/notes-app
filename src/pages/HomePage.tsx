ea0f24927f5e2c7fed4dafd7b0f0ee0bfd9fb95e
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-26 23:58:44 +0300

    style the homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,43 @@
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 import NotesList from "../components/NotesList";
 import { useNotesContext } from "../context/NotesContext";
+
+const Container = styled.div`
+  margin: 0 0 20px;
+
+  h1 {
+    margin-bottom: 10px;
+    text-align: center;
+    color: blue;
+  }
+
+  > button {
+    display: block;
+    margin: 0 auto;
+    color: green;
+    padding: 5px 20px;
+    font-size: 14px;
+    font-weight: bold;
+    cursor: pointer;
+    margin-bottom: 20px;
+  }
+`;
+const Divider = styled.div`
+  width: 100%;
+  border-bottom: 3px solid blue;
+`;
 
 export default function HomePage() {
   const { notes } = useNotesContext();
   const navigate = useNavigate();
   return (
-    <div>
+    <Container>
       <h1>All Notes</h1>
       <button onClick={() => navigate("/new")}>New</button>
-
+      <Divider />
       <NotesList notes={notes} />
-    </div>
+    </Container>
   );
 }
