9e1e816ece6aaf79dab5e59a7048d102cc6663d1
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-27 19:35:41 +0300

    style the back button

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,7 +1,24 @@
 import { useParams, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 import { useNotesContext } from "../context/NotesContext";
 import Note from "../components/Note";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const BackBtn = styled.button`
+  padding: 5px 15px;
+  background-color: black;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+  margin: 0 auto;
+  margin-bottom: 30px;
+`;
 
 export default function DetailsPage(): JSX.Element {
   const { id } = useParams();
@@ -12,9 +29,9 @@ export default function DetailsPage(): JSX.Element {
   const noteIdx = notes.findIndex((n) => n.id === id);
   const note = notes[noteIdx];
   return (
-    <>
+    <Container>
       <Note {...note} />
-      <button onClick={() => navigate(-1)}>Back</button>
-    </>
+      <BackBtn onClick={() => navigate(-1)}>Back</BackBtn>
+    </Container>
   );
 }
