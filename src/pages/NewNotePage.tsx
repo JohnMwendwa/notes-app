2ee10692fbc5b59d1e95d10a6ef7c1f32955e171
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-27 21:25:31 +0300

    style the NewNotePage

--- src/pages/NewNotePage.tsx
+++ src/pages/NewNotePage.tsx
@@ -1,12 +1,25 @@
 import NoteForm from "../components/NoteForm";
+import styled from "styled-components";
+
 import { useNotesContext } from "../context/NotesContext";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 0 30px;
+`;
+
+const Title = styled.h1`
+  color: green;
+`;
 
 export default function NewNotePage() {
   const { onCreateNote } = useNotesContext();
   return (
-    <>
-      <h1>Create Note</h1>
+    <Container>
+      <Title>Create Note</Title>
       <NoteForm onSubmit={onCreateNote} />
-    </>
+    </Container>
   );
 }
